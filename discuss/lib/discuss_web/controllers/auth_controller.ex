678861baccf4a957fccde87dfa15d37063678819
Cedric Creusot <cedric.creusot@gmail.com>
2020-10-06 08:33:35 +0200

    OAuth working, login and logout is working with session

--- discuss/lib/discuss_web/controllers/auth_controller.ex
+++ discuss/lib/discuss_web/controllers/auth_controller.ex
@@ -5,7 +5,7 @@ defmodule DiscussWeb.AuthController do
     alias Discuss.User
     alias Discuss.Repo
 
-    def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
+    def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
         user_params = %{
             token: auth.credentials.token,
             email: auth.info.email,
@@ -16,17 +16,23 @@ defmodule DiscussWeb.AuthController do
         signin(conn, changeset)
     end
 
+    def signout(conn, _params) do
+        conn
+        |> configure_session(drop: true)
+        |> redirect(to: Routes.topic_path(conn, :index))
+    end
+
     defp signin(conn, changeset) do
         case insert_or_update_user(changeset) do
             {:ok, user} ->
                 conn
                 |> put_flash(:info, "Welcome back!")
                 |> put_session(:user_id, user.id)
-                |> redirect to: Routes.topic_path(conn, :index)
-            {:error, reason} ->
+                |> redirect(to: Routes.topic_path(conn, :index))
+            {:error, _reason} ->
                 conn
                 |> put_flash(:error, "Error signin in")
-                |> redirect to: Routes.topic_path(conn, :index)
+                |> redirect(to: Routes.topic_path(conn, :index))
         end
     end
 
