6d9299dfa996db85526050e781e596a0edf05f19
Cedric Creusot <cedric.creusot@gmail.com>
2020-10-13 09:21:58 +0200

    End of section 13 with adding user in socket to identify it and add the comments association

--- discuss/lib/discuss_web/channels/comments_channel.ex
+++ discuss/lib/discuss_web/channels/comments_channel.ex
@@ -1,22 +1,24 @@
 defmodule DiscussWeb.CommentsChannel do
     use Phoenix.Channel
 
-    alias Discuss.{Topic, Comment}
+    alias Discuss.{Topic, User, Comment}
     alias Discuss.Repo
 
     def join("comments:" <> topic_id, _params, socket) do
         topic_id = String.to_integer(topic_id)
         topic = Topic 
             |> Repo.get(topic_id)
-            |> Repo.preload(:comments)
+            |> Repo.preload(comments: [:user])
         
         {:ok, %{comments: topic.comments}, assign(socket, :topic, topic)}
     end
 
     def handle_in(name, %{"content" => content}, socket) do
         topic = socket.assigns.topic
+        user_id = socket.assigns.user_id
+
         changeset = topic
-            |> Ecto.build_assoc(:comments)
+            |> Ecto.build_assoc(:comments, user_id: user_id)
             |> Comment.changeset(%{content: content})
 
         case Repo.insert(changeset) do
